3c228da3608a4ec78388ea2ec2248fa9b4b1a3f3
Carushi <trumpet-lambda@hotmail.co.jp>
2015-05-25 19:55:50 +0900

    add linear sieve in golang

---
+++ linear_sieve.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"os"
+)
+
+func LinearSieve(n int) []int {
+	arr := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		arr[i] = i
+	}
+	prime := []int{}
+	for i := 2; i <= n; i++ {
+		if arr[i] == i {
+			prime = append(prime, i)
+		}
+		for _, j := range prime {
+			m := i * j
+			if j <= arr[i] && m <= n {
+				arr[m] = j
+			} else {
+				break
+			}
+		}
+	}
+	return prime
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("No arguments.")
+		return
+	}
+	num, _ := strconv.Atoi(os.Args[1])
+	if num < 2 {
+		return
+	}
+    fmt.Println(LinearSieve(num))
+}
